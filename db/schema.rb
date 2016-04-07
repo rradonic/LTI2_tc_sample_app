fd6d1c3dd4ec1ad10dfa624bd59df27bd26a7dc4
Ranko Radonić <ranko.radonic@gmx.com>
2016-04-07 14:33:10 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505213019) do
+ActiveRecord::Schema.define(version: 20151111000000) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 20150505213019) do
     t.datetime "updated_at"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admin_users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20150505213019) do
     t.string   "mentor"
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
   create_table "courses", force: true do |t|
     t.string   "course_label"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 20150505213019) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "lti2_tc_deployment_requests", ["tc_profile_guid"], name: "index_deployment_requests_on_tc_profile_guid", unique: true, using: :btree
+  add_index "lti2_tc_deployment_requests", ["tc_profile_guid"], name: "index_deployment_requests_on_tc_profile_guid", unique: true
 
   create_table "lti2_tc_links", force: true do |t|
     t.string   "resource_link_label"
@@ -132,7 +132,7 @@ ActiveRecord::Schema.define(version: 20150505213019) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "lti2_tc_tool_consumer_profiles", ["tc_profile_guid"], name: "index_tool_consumer_profiles_on_tc_profile_guid", unique: true, using: :btree
+  add_index "lti2_tc_tool_consumer_profiles", ["tc_profile_guid"], name: "index_tool_consumer_profiles_on_tc_profile_guid", unique: true
 
   create_table "lti2_tc_tool_settings", force: true do |t|
     t.integer "tool_id"
@@ -157,16 +157,6 @@ ActiveRecord::Schema.define(version: 20150505213019) do
     t.datetime "updated_at",                null: false
   end
 
-  add_index "lti2_tc_tools", ["key"], name: "index_tools_on_key", unique: true, using: :btree
-
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
+  add_index "lti2_tc_tools", ["key"], name: "index_tools_on_key", unique: true
 
 end
